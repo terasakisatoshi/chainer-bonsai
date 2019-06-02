3983a9446a61531ebbb76d8b78f6bfe5c0366baa
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2019-06-02 22:44:30 +0900

    add example function node

---
+++ officialTutorial/functionNone/expadd.py
@@ -0,0 +1,79 @@
+import chainer
+import chainer.functions as F
+from chainer import FunctionNode, Variable
+from chainer.backends import cuda
+import numpy as np
+
+
+class NaiveExpAdd(FunctionNode):
+    """
+    define function node
+    z := f(x,y) = exp(x) + exp(y)
+    """
+
+    def forward_cpu(self, inputs):
+        self.retain_inputs((0, 1))
+        x, y = inputs
+        z = np.exp(x) + np.exp(y)
+        return z,
+
+    def forward_gpu(self, inputs):
+        self.retain_inputs((0, 1))
+        cp = cuda.cupy
+        x, y = inputs
+        z = cp.exp(x) + cp.exp(y)
+        return z,
+
+    def backward(self, target_input_indexes, grad_outputs):
+        x, y = self.get_retained_inputs()
+        gz, = grad_outputs
+        gx = F.exp(x) * gz
+        gy = F.exp(y) * gz
+        return gx, gy
+
+
+class ExpAdd(FunctionNode):
+    """
+    define function node
+    z := f(x,y) = exp(x) + exp(y)
+    """
+
+    def forward(self, inputs):
+        self.retain_inputs((0, 1))
+        xp = chainer.backend.get_array_module(*inputs)
+        x, y = inputs
+        z = xp.exp(x) + xp.exp(y)
+        return z,
+
+    def backward(self, target_input_indexes, grad_outputs):
+        x, y = self.get_retained_inputs()
+        gz, = grad_outputs
+        gx = F.exp(x) * gz
+        gy = F.exp(y) * gz
+        return gx, gy
+
+
+def naive_expadd(x, y):
+    z, = NaiveExpAdd().apply((x, y))
+    return z
+
+
+def expadd(x, y):
+    z, = ExpAdd().apply((x, y))
+    return z
+
+
+def main():
+    x = Variable(np.random.uniform(-1, 1, (3, 2)).astype(np.float32))
+    y = Variable(np.random.uniform(-1, 1, (3, 2)).astype(np.float32))
+    naive_expadd(x, y)
+    z = expadd(x, y)
+    xp = chainer.backend.get_array_module(*(x, y))
+    z.grad = xp.ones(z.shape, dtype=np.float32)
+    z.backward()
+    assert (x.grad == xp.exp(x.array)).all()
+    assert (y.grad == xp.exp(y.array)).all()
+
+
+if __name__ == '__main__':
+    main()
---
+++ officialTutorial/functionNone/muladd.py
@@ -0,0 +1,60 @@
+import chainer
+from chainer import FunctionNode, Variable
+import numpy as np
+
+
+class MulAdd(FunctionNode):
+    """
+    f(x, y, z) = x * y + z
+    """
+
+    def forward_cpu(self, inputs):
+        # do forward computation on CPU
+        #  Note that all arrays appearing in forward_cpu method are `numpy.ndarray`
+        x, y, z = inputs
+        self.retain_inputs((0, 1))
+        w = x * y + z
+        # Be careful to return a tuple even if you have just one array or Variable to return.
+        return w,
+
+    def forward_gpu(self, inputs):
+        """
+        In forward_gpu method, arrays are of type cupy.ndarray.
+        We use arithmetic operators defined for this class.
+        These operators implement the basic elementwise arithmetics.
+        """
+        x, y, z = inputs
+        self.retain_inputs((0, 1))
+        w = x * y + z
+        return w,
+
+    def backward(self, target_input_indexes, grad_outputs):
+        x, y = self.get_retained_inputs()
+        gw, = grad_outputs
+        gx = y * gw
+        gy = x * gw
+        gz = gw
+        return gx, gy, gz
+
+
+def muladd(x, y, z):
+    w, = MulAdd().apply((x, y, z))
+    return w
+
+
+def main():
+    x = Variable(np.random.uniform(-1, 1, (3, 2)).astype(np.float32))
+    y = Variable(np.random.uniform(-1, 1, (3, 2)).astype(np.float32))
+    z = Variable(np.random.uniform(-1, 1, (3, 2)).astype(np.float32))
+    inputs = (x, y, z)
+    w = muladd(*inputs)
+    xp = chainer.backend.get_array_module(*inputs)
+    w.grad = xp.ones(w.shape, dtype=np.float32)
+    w.backward()
+    assert (x.grad == y.array).all()
+    assert (y.grad == x.array).all()
+    assert (z.grad == xp.ones(z.shape)).all()
+
+
+if __name__ == '__main__':
+    main()
